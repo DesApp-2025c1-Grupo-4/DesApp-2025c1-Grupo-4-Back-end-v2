1d908dacfb83310e23c7ad6bf519c31e743c785a
LuisHerenu11 <ElCondor_1106@hotmail.com>
2025-05-20 01:00:21 -0300

    fix/ index router and IdValidator

--- middleware/idValidador.js
+++ middleware/idValidador.js
@@ -3,15 +3,12 @@ const mongoose = require('mongoose')
 const validarPatenteVehiculo = (Modelo) => {
     return async (req, res, next) => {
         const { patente } = req.params;
-
         try {
             // Buscar la patente en la base de datos
             const vehiculo = await Modelo.findOne({ patente });
-
             if (!vehiculo) {
                 return res.status(404).json({ error: "Patente no encontrada" });
             }
-
             req.vehiculo = vehiculo; // Guarda el vehículo encontrado en la request
             next(); // Continúa con el siguiente middleware o controlador
         } catch (error) {
@@ -23,15 +20,12 @@ const validarPatenteVehiculo = (Modelo) => {
 const validarId = (Modelo) => {
     return async (req, res, next) => {
         const { _id } = req.params;
-
         try {
             // Buscar el id en la base de datos
             const id = await Modelo.findOne({ _id });
-
             if (!id) {
                 return res.status(404).json({ error: "id no encontrada" });
             }
-
             req.id = id; // Guarda el id encontrado en la request
             next(); // Continúa con el siguiente middleware o controlador
         } catch (error) {
@@ -39,23 +33,16 @@ const validarId = (Modelo) => {
         }
     };
 };
-console.log(validarPatenteVehiculo);
-
-
-module.exports = { validarId,validarPatenteVehiculo };
 
 const validarCuilChofer = (Modelo) => {
     return async (req, res, next) => {
         const { cuil } = req.params;
-
         try {
             //Buscar el cuil en la base de datos
             const chofer = await Modelo.findOne({ cuil });
-
             if(!chofer) {
                 return res.status(404).json({error: "CUIL no encontrado"});
             }
-
             req.chofer = chofer; //Guarda el chofer encontrado en la request
             next(); //Continúa con el siguiente middleware o controlador
         } catch (error) {
@@ -64,4 +51,4 @@ const validarCuilChofer = (Modelo) => {
     };
 };
 
-module.exports = {validarId, validarCuilChofer};
+module.exports = {validarId, validarPatenteVehiculo, validarCuilChofer};
