d49d23ce563866430f6214c6f829f2121928dc98
ErikaLois <erikapatricia.lois@estudiantes.unahur.edu.ar>
2025-05-23 21:02:04 -0300

    Se completa el schema de depósito

--- schemas/deposito.schema.js
+++ schemas/deposito.schema.js
@@ -7,7 +7,18 @@ const depositoSchema = Joi.object().keys(
         "string.min": "El tipo debe tener como mínimo {#limit} caracteres",
         "string.max": "El tipo debe tener como máximo {#limit} caracteres",
         "string.empty": "El tipo no puede ser vacía"
-    })
+    }),
+
+    horarios: Joi.string().required().messages({
+        "any.required": "El horario es requerido",
+        "string.empty": "El horario no puede ser vacío"
+    }),
+    contacto: Joi.number().integer().required().messages({
+        "any.required": "El contacto es requerido",
+        "number.base": "El contacto debe ser un número entero"
+    }),
+    
+    localizacion: Joi.number().optional()
 
 }).unknown(false).messages ({
     'object.unknown': 'El atributo {#label} no está permitido.'
