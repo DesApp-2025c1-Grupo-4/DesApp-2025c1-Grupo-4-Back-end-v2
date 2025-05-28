63821393123ff1dd0d0f0dbac0eb9a2eab9dd428
ErikaLois <erikapatricia.lois@estudiantes.unahur.edu.ar>
2025-05-28 18:59:14 -0300

    Se crean gets de empresa

--- schemas/empresa.schema.js
+++ schemas/empresa.schema.js
@@ -2,11 +2,11 @@ const Joi = require('joi')
 
 const empresaSchema = Joi.object().keys(
     {
-    cuit: Joi.bigint().min(11).max(11).required().messages({
+    cuit: Joi.string().min(11).max(11).required().messages({
         "any.required":"nombre es requerido",
-        "bigint.min": "El CUIT debe tener como mínimo {#limit} caracteres",
-        "bigint.max": "El CUIT debe tener como máximo {#limit} caracteres",
-        "bigint.empty": "El CUIT no puede ser vacio"
+        "string.min": "El CUIT debe tener como mínimo {#limit} caracteres",
+        "string.max": "El CUIT debe tener como máximo {#limit} caracteres",
+        "string.empty": "El CUIT no puede ser vacio"
     })
 
 }).unknown(false).messages ({
