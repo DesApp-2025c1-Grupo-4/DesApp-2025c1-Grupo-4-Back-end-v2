65dc5e607dd9a569331c5241bb5b63b006077d7b
LuisHerenu11 <ElCondor_1106@hotmail.com>
2025-06-07 13:01:46 -0300

    swagger config

--- src/app.js
+++ src/app.js
@@ -1,24 +1,35 @@
-const express = require('express')
+const express = require('express');
 const routes = require('./routers/index');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
-require('dotenv').config()
-const { connectToDatabase } = require('./db/server')
-const initialEmpresa = require('./seeders/initialseeders')
+require('dotenv').config();
+const { connectToDatabase } = require('./db/server');
+const initialEmpresa = require('./seeders/initialseeders');
 
-const app = express()
-app.use(express.json())
-app.use(routes)
+const app = express();
+app.use(express.json());
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3000;
 
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+if (process.env.USE_SWAGGER === 'true') {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    console.log('✅ Swagger habilitado en /api-docs');
+} else {
+    console.log('ℹ️ Swagger deshabilitado');
+}
 
-app.listen(PORT, async () => {
-    await connectToDatabase()
-    await initialEmpresa()
-    console.log('Aplicación corriendo en el puerto: ', PORT)
-    const open = (await import('open')).default;
-    //await open(`http://localhost:${PORT}/api-docs`);
-})
+app.use(routes);
+
+const startServer = async () => {
+    await connectToDatabase();
+    await initialEmpresa();
+    console.log('Aplicación corriendo en el puerto:', PORT);
+
+    if (process.env.USE_SWAGGER === 'true') {
+        const open = (await import('open')).default;
+        await open(`http://localhost:${PORT}/api-docs`);
+    }
+};
+
+app.listen(PORT, startServer);
 
