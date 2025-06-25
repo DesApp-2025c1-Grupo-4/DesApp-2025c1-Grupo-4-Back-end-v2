b937208aee99a471e17e156ccc9636c43ef05e3f
ErikaLois <erikapatricia.lois@estudiantes.unahur.edu.ar>
2025-06-24 22:26:50 -0300

    Se agrega validación a chofer para que solo puedan asignarse vehículos de la empresa a la cual pertenece

--- src/routers/chofer.routes.js
+++ src/routers/chofer.routes.js
@@ -1,8 +1,8 @@
 const { Router } = require('express')
-const { Chofer } = require('../models')
+const { Chofer, Vehiculo } = require('../models')
 const { ChoferController } = require('../controllers')
-const choferSchema = require('../schemas/chofer.schema')
-const { validarId, validarCuilChofer, validarCampoDuplicado } = require('../middleware/idValidador')
+const {choferSchema} = require('../schemas/chofer.schema')
+const { validarId, validarCuilChofer, validarCampoDuplicado, validarVehiculoDeEmpresa } = require('../middleware/idValidador')
 const schemasValidador  = require('../middleware/schemasValidador')
 
 const routes = Router()
@@ -10,8 +10,8 @@ const routes = Router()
 routes.get('/', ChoferController.getChoferes)
 routes.get('/:_id', validarId(Chofer), ChoferController.getChoferById)
 routes.get('/:cuil/cuil', validarCuilChofer(Chofer), ChoferController.getChoferByCuil)
-routes.post('/', schemasValidador(choferSchema), validarCampoDuplicado(Chofer, 'cuil', 'chofer', 'un'), ChoferController.addChofer);
-routes.put('/:_id', validarId(Chofer), schemasValidador(choferSchema), validarCampoDuplicado(Chofer, 'cuil', 'chofer', 'un'), ChoferController.updateChofer);
+routes.post('/', schemasValidador(choferSchema), validarCampoDuplicado(Chofer, 'cuil', 'chofer', 'un'), validarVehiculoDeEmpresa(Vehiculo), ChoferController.addChofer);
+routes.put('/:_id', validarId(Chofer), schemasValidador(choferSchema), validarCampoDuplicado(Chofer, 'cuil', 'chofer', 'un'), validarVehiculoDeEmpresa(Vehiculo), ChoferController.updateChofer);
 routes.patch('/:_id/delete', validarId(Chofer), ChoferController.softDeleteChofer)
 
 module.exports = routes
