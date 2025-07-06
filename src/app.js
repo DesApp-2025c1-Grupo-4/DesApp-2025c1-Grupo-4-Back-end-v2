335c4e18fc7febc857d9694487029a177d1fcb12
MarceloTorres88 <russo1988torres@gmail.com>
2025-07-06 20:17:46 -0300

    fix modificacion licencia

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const routes = require('./routers');
 const initialEmpresa = require('./seeders/initialseeders');
 
 const app = express();
-app.use(express.json());
-
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 const PORT = process.env.PORT ?? 3000;//sin .env corre en 3000
 const REMOTE_URI = process.env.MONGO_URL;//Atlas
 const LOCAL_URI = 'mongodb://localhost:27017/desapp';//Si falla el puerto o la conexión.
